6a4ffee4d566ab2673fc501672fadd5e39d659da
Afsana Akter Eva <afsanaeva05@gmail.com>
2025-12-24 12:57:03 +0600

    update setting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 
-import {  View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1  justify-center items-center">
-      {/* <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text> */}
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
   );
 }
